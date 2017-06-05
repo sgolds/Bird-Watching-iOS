567145ccccf08f433873e4e8cd275443a44ceb68
sgolds <sgolds@sandiego.edu>
2017-06-04 20:47:50 -0700

    Constants

---
+++ Utils/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  Bird Watching
+//
+//  Created by Sten Golds on 7/13/16.
+//  Copyright © 2016 Sten Golds. All rights reserved.
+//
+
+import UIKit
+
+//colors
+let semitransparentWhite = UIColor.white.withAlphaComponent(0.7)
+let greenForPin = UIColor(red: 46.0/255.0, green: 204.0/255.0, blue: 113.0/255.0, alpha: 1.0)
+let redForPin = UIColor(red: 231.0/255.0, green: 76.0/255.0, blue: 60.0/255.0, alpha: 1.0)
+
+
+//keys
+let KEY_USER_EMAIL = "user email"
+
+//segues
+let LOGGED_IN_SEGUE = "loggedIn"
+let DISPLAY_POST_SEGUE = "toPostDisplay"
+let COMMENT_SEGUE = "toAddComment"
+
